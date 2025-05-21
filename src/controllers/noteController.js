1e736736bb8ea6b363f6a740f5f8fceb9ac46112
Naman Singh <Naman.singh@acmeintech.in>
2025-05-21 16:29:28 +0530

    Notebook Backend

--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -1,40 +1,69 @@
-const noteService = require('../services/noteService');
+const Note = require("../models/noteModel");
 
 const getNotes = async (req, res) => {
   try {
-    const notes = await noteService.getNotes(req.user._id);
+    const notes = await Note.find({ user: req.user._id });
     res.json(notes);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
 const createNote = async (req, res) => {
   try {
     const { title, content } = req.body;
-    const note = await noteService.createNote(req.user._id, title, content);
+
+    const note = await Note.create({
+      user: req.user._id,
+      title,
+      content,
+    });
+
     res.status(201).json(note);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
 const updateNote = async (req, res) => {
   try {
     const { title, content } = req.body;
-    const note = await noteService.updateNote(req.params.id, req.user._id, title, content);
-    res.json(note);
+    const note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    if (note.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    note.title = title;
+    note.content = content;
+
+    const updatedNote = await note.save();
+    res.json(updatedNote);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
 const deleteNote = async (req, res) => {
   try {
-    const result = await noteService.deleteNote(req.params.id, req.user._id);
-    res.json(result);
+    const note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    if (note.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    await Note.findByIdAndDelete(req.params.id);
+    res.json({ message: "Note removed" });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,22 +1,57 @@
-const userService = require('../services/userService');
-
-const registerUser = async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const userData = await userService.registerUser(username, password);
-    res.status(201).json(userData);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
+const User = require("../models/userModel");
+const generateToken = require("../utils/generateToken");
 
 const loginUser = async (req, res) => {
   try {
-    const { username, password } = req.body;
-    const userData = await userService.loginUser(username, password);
-    res.json(userData);
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401);
+      throw new Error("Invalid email or password");
+    }
   } catch (error) {
-    res.status(401).json({ error: error.message });
+    res.status(401).json({ message: error.message });
+  }
+};
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400);
+      throw new Error("User already exists");
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 };
 
