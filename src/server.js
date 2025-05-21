53416d61444103ee7f59c58366911ca8d536255a
Naman Singh <Naman.singh@acmeintech.in>
2025-05-21 13:31:10 +0530

    Notebook Backend

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "https://notebook-frontend-murex.vercel.app/",
+    origin: "https://notebook-frontend-murex.vercel.app",
     credentials: true,
   })
 );
